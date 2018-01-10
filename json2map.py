ec8394040fcf9f5afcb5c4ac90e84062311075ab
Sasha Ames <amysash2006@gmail.com>
2018-01-10 10:43:50 -0800

    update facets

--- json2map.py
+++ json2map.py
@@ -3,7 +3,7 @@ import json, sys
 obj = json.load(open(sys.argv[1]))
 
 
-basic_facets = ["project", "institution_id", "data_type", "target_mip", "time_frequency", "source", "variable_id", "grid_label"]
+basic_facets = ["mip_era", "activity_id", "target_mip",  "institution_id", "realm", "frequency", "source_id", "variable_id", "grid_label"]
 
 
 dset_arr =[]
