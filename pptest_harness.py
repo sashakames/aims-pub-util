352f277bad279b1af62f34fb8026491346a56868
Sasha Ames <amysash2006@gmail.com>
2018-01-16 14:20:45 -0800

    fix indents, typos, etc

--- pptest_harness.py
+++ pptest_harness.py
@@ -6,49 +6,44 @@ import argparse
 import re
 
 
-def proc_path(path)
+def proc_path(path):
 
 
 	cf = Cdunif.CdunifFile(path, 'r')
 
-    try:
-        table = getattr(cf, 'table_id')
-    except:
-        raise ESGPublishError("File %s missing required table_id global attribute"%f)
+	try:
+	    table = getattr(cf, 'table_id')
+	except:
+	    raise ESGPublishError("File %s missing required table_id global attribute"%f)
 
-    try:
-        variable_id = getattr(cf, 'variable_id')
-    except:
-        raise ESGPublishError("File %s missing required variable_id global attribute"%f)
+	try:
+	    variable_id = getattr(cf, 'variable_id')
+	except:
+	    raise ESGPublishError("File %s missing required variable_id global attribute"%f)
 
+	table_file = sys.argv[1] + '/CMIP6_' + table + '.json'
+	fakeargs = [ '--variable', variable_id, table_file ,f]
+	parser = argparse.ArgumentParser(prog='esgpublisher')
+	parser.add_argument('--variable')
+	parser.add_argument('cmip6_table', action=validator.JSONAction)
+	parser.add_argument('infile', action=validator.CDMSAction)
+	parser.add_argument('outfile',
+	        nargs='?',
+	        help='Output file (default stdout)',
+	        type=argparse.FileType('w'),
+	        default=sys.stdout)
+	args = parser.parse_args(fakeargs)
 
+	#        print "About to CV check:", f
+	process = validator.checkCMIP6(args)
 
-    table_file = sys.argv[1] + '/CMIP6_' + table + '.json'
-    fakeargs = [ '--variable', variable_id, table_file ,f]
-    parser = argparse.ArgumentParser(prog='esgpublisher')
-    parser.add_argument('--variable')
-    parser.add_argument('cmip6_table', action=validator.JSONAction)
-    parser.add_argument('infile', action=validator.CDMSAction)
-    parser.add_argument('outfile',
-            nargs='?',
-            help='Output file (default stdout)',
-            type=argparse.FileType('w'),
-            default=sys.stdout)
-    args = parser.parse_args(fakeargs)
-
-#        print "About to CV check:", f
-
-    try:
-        process = validator.checkCMIP6(args)
-
-        args.infile = cf
-        process.ControlVocab(args)
-        args.infile.close()
+	args.infile = cf
+	process.ControlVocab(args)
+	args.infile.close()
 
 
 def main():
-
-	if len(sys.argv < 2):
+	if len(sys.argv) < 2:
 		print "usage <command> | python pptest_harness.py <cmor-table-path>"
 		exit(0)
 
