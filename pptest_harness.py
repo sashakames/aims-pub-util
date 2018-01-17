e104d269f70d47e859c9783fd0e26b8cf46feb86
sashakames <sasha@llnl.gov>
2018-01-17 10:17:16 -0800

    works but error issue

--- pptest_harness.py
+++ pptest_harness.py
@@ -6,24 +6,26 @@ import argparse
 import re
 
 
-def proc_path(path):
+def proc_path(validator, path):
 
 
 	cf = Cdunif.CdunifFile(path, 'r')
+
+	table = ''
 
 	try:
 	    table = getattr(cf, 'table_id')
 	except:
 	    raise ESGPublishError("File %s missing required table_id global attribute"%f)
-
+	variable_id =''
 	try:
 	    variable_id = getattr(cf, 'variable_id')
 	except:
 	    raise ESGPublishError("File %s missing required variable_id global attribute"%f)
 
 	table_file = sys.argv[1] + '/CMIP6_' + table + '.json'
-	fakeargs = [ '--variable', variable_id, table_file ,f]
-	parser = argparse.ArgumentParser(prog='esgpublisher')
+	fakeargs = [ '--variable', variable_id, table_file , path]
+	parser = argparse.ArgumentParser(prog='pptest_harness')
 	parser.add_argument('--variable')
 	parser.add_argument('cmip6_table', action=validator.JSONAction)
 	parser.add_argument('infile', action=validator.CDMSAction)
@@ -35,10 +37,14 @@ def proc_path(path):
 	args = parser.parse_args(fakeargs)
 
 	#        print "About to CV check:", f
+
 	process = validator.checkCMIP6(args)
 
 	args.infile = cf
-	process.ControlVocab(args)
+	try:
+		process.ControlVocab(args)
+	except KeyboardInterrupt as e:
+		print "Error with ",path, str(e)
 	args.infile.close()
 
 
@@ -49,7 +55,7 @@ def main():
 
 	for line in sys.stdin:
 
-		proc_path(line.rstrip())
+		proc_path(PrePARE.PrePARE  , line.rstrip())
 
 
 if (__name__ == '__main__'):
