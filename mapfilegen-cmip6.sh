a2653140839f6c9bd4adace3612ad3afb2a21ee2
Sasha Ames <amysash2006@gmail.com>
2018-10-31 15:56:17 -0700

    add loops

--- mapfilegen-cmip6.sh
+++ mapfilegen-cmip6.sh
@@ -1,9 +1,20 @@
 # Input - a list of directories
 
+input_dir=/p/user_pub/publish-queue/CMIP6-list-todo
+done_dir=/p/user_pub/publish-queue/CMIP6-list-done
+
+
 cmip6_dirlst=$1
 
-for n in `cat $cmip6_dirlst`
+for m in `ls $cmip6_dirlst` 
+do
 
-do time esgmapfile -i ../ini --project cmip6 --outdir /p/user_pub/CMIP6-maps-todo --max-processes 32 $n 
+	echo BEGIN $m
+	for n in `cat $m` 
+		do 
+		echo RUN $n
+		time esgmapfile -i ../ini --project cmip6 --outdir /p/user_pub/CMIP6-maps-todo --max-processes 32 $n 
 
+	done
+	mv $m $done_dir
 done
