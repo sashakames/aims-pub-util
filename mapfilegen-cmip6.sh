f438a0ffcb3bd510b406e5b68d6eb9abb676cacc
Sasha Ames <ames4@llnl.gov>
2018-11-01 10:33:02 -0700

    fix some things

--- mapfilegen-cmip6.sh
+++ mapfilegen-cmip6.sh
@@ -5,13 +5,11 @@ input_dir=/p/user_pub/publish-queue/CMIP6-list-todo
 done_dir=/p/user_pub/publish-queue/CMIP6-list-done
 
 
-cmip6_dirlst=$1
-
-for m in `ls $cmip6_dirlst` 
+for m in `ls $input_dir` 
 do
 
 	echo BEGIN $m
-	for n in `cat $m` 
+	for n in `cat $input_dir/$m` 
 		do 
 		echo RUN $n
 		time esgmapfile -i ../ini --project cmip6 --outdir /p/user_pub/CMIP6-maps-todo --max-processes 32 $n 
