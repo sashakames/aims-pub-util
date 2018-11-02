a1f6466e9dcbe1f90d348da104ce39c91af33f3f
Alexander K. Ames <ames4@aimsdtn5.llnl.gov>
2018-11-02 15:33:25 -0700

    initial plus some updating

---
+++ mv-from-scratch.sh
@@ -0,0 +1,37 @@
+sg synda
+
+SRCBASE=/p/css03/scratch/CMIP6
+TARGETPATH=/p/user_pub/publish-queue/CMIP6-list-todo
+
+PREFIX=$1
+
+
+SRCPATH="${SRCBASE}/${PREFIX}"
+DESTPATH=`echo $SRCPATH | sed s/scratch/esgf_publish/`
+DOTDELIM=`echo $PREFIX/$2 | sed s/'\/'/'\.'/g`
+
+ITEM=$SRCPATH/$2
+
+if [ ! -d $ITEM ] ; then
+
+echo source path $ITEM does not exist!
+exit
+fi
+
+mkdir -m=775 -p $DESTPATH 
+
+
+mv $ITEM $DESTPATH
+
+if [ ! -d $DESTPATH ] ; then
+
+ echo dest path $DESTPATH does not exist! 
+ exit
+fi
+
+
+ls -d $DESTPATH/$2/*/* > $TARGETPATH/$DOTDELIM.lst
+
+chgrp climatew $TARGETPATH/$DOTDELIM.lst
+chmod a+r $TARGETPATH/$DOTDELIM.lst
+chmod g+wrx $TARGETPATH/$DOTDELIM.lst
