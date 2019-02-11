e0061131738e3f95a86944f8223887616f248c54
Sasha Ames <ames4@aims3.llnl.gov>
2019-02-11 15:28:00 -0800

    update fix-up

--- fix-up.sh
+++ fix-up.sh
@@ -1,18 +1,21 @@
 tds_cats=/esg/content/thredds/esgcet
-
 rm tmp.txt
 
-for i in range $1 $2 ; do
+cert="/export/ames4/.globus/certificate-file"
 
-	cat_dir=tds_cats/$i
+
+for i in `seq $1 $2` ; do
+
+	cat_dir=$tds_cats/$i
 
 	for n in $cat_dir/*.xml ; do
 
 		val=`grep -c 'gsiftp://aims3.llnl.gov' $n`
 
 		if [ $val -gt 0 ] ; then
-			sed -i s/'gsiftp\:\/\/aims3'/'gsiftp\:\/\/aimsdtn3'/g
-			echo $n >> tmp.txt
+		    sed -i s/'gsiftp\:\/\/aims3'/'gsiftp\:\/\/aimsdtn3'/g $n
+		    bn=`basename $n`
+		    echo $i/$bn >> tmp.txt
 		fi
 
 	done
@@ -21,7 +24,10 @@ done
 
 esgpublish --project cmip6 --thredds-reinit
 
-for $n in `cat tmp.txt` ; do
-	
-		
+for xml in `cat tmp.txt` ; do
+    wget --no-check-certificate --ca-certificate $cert  --certificate $cert --private-key $cert --verbose --post-data="uri=http://aims3.llnl.gov/thredds/catalog/esgcet/$xml&metadataRepositoryType=THREDDS" https://esgf-node.llnl.gov/esg-search/ws/harvest    
+    cat harvest
+    echo
+    rm harvest
+
 done
