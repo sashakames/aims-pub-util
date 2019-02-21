f60c7bf0f36e1c7440035f2bbd374a604641e9c1
Sasha Ames <ames4@llnl.gov>
2019-02-20 16:09:51 -0800

    ini file spec

--- mapfilegen-cmip6.sh
+++ mapfilegen-cmip6.sh
@@ -3,6 +3,7 @@
 input_dir=$1
 m=$2
 
+inidir=/export/ames4/pub/ini
 
 for n in `cat $input_dir/$m` 
 	do 
@@ -11,7 +12,7 @@ for n in `cat $input_dir/$m`
 		echo missing perms or mount [FAIL] $m
 		exit 1
 	fi
-	time esgmapfile -i ../ini --project cmip6 --outdir /p/user_pub/CMIP6-maps-todo --max-processes 16 $n 
+	time esgmapfile -i $inidir --project cmip6 --outdir /p/user_pub/CMIP6-maps-todo --max-processes 16 $n 
 
 	if [ $? -ne 0 ]; then
 		echo esgmapfile $n [FAIL] $m
