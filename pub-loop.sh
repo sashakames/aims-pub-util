b9a8db3db9c4ed9bdf1bf43437662311abf32d0a
Sasha Ames <ames4@aims3.llnl.gov>
2019-04-10 14:57:06 -0700

    some correction

--- pub-loop.sh
+++ pub-loop.sh
@@ -1,13 +1,13 @@
 export UVCDAT_ANONYMOUS_LOG=no
-for i in `seq 1 5000`  
-stop=`cat /tmp/pub_status`
-
-if [ $stop == "true" ] ; then
-    echo Received Stop Notification, exiting 
-    exit
-fi 
-
-do time bash ../aims-pub-util/publish-cmip6-replica.sh /p/user_pub/CMIP6-maps-todo 120
-sleep 300 
+for i in `seq 1 5000`  ; do
+    stop=`cat /tmp/pub_status`
+    
+    if [ $stop == "true" ] ; then
+	echo Received Stop Notification, exiting 
+	exit
+    fi 
+    
+    time bash ../aims-pub-util/publish-cmip6-replica.sh /p/user_pub/CMIP6-maps-todo 120
+    sleep 300 
 
 done
