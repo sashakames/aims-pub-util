d2f9e5673f22ca8d2256f03a7f8a27928b059c7b
Sasha Ames <amysash2006@gmail.com>
2019-04-12 15:01:09 -0700

    add stop and counter

--- mapfilegen-cmip6.sh
+++ mapfilegen-cmip6.sh
@@ -7,21 +7,30 @@ outdir=/p/user_pub/CMIP6-maps-todo
 
 inidir=/export/ames4/pub/ini
 
+i=1
+
 for n in `cat $input_dir/$m` 
+
+    stop=`cat /tmp/map_status`
+    
+    if [ $stop == "true" ] ; then
+    	echo Received Stop Notification, exiting before $m $i
+
 	do 
 	echo RUN $n
 	if [ ! -d $n ] ; then 
-		echo missing perms or mount [FAIL] $m
-		echo missing perms or mount [FAIL] $m | sendmail ames4@llnl.gov
+		echo missing perms or mount [FAIL] $m $i 
+		echo missing perms or mount [FAIL] $m $i | sendmail ames4@llnl.gov
 		exit 1
 	fi
 	esgmapfile -i $inidir --project cmip6 --outdir $outdir --max-processes 8 $n 
 
 	if [ $? -ne 0 ]; then
-		echo esgmapfile $n [FAIL] $m
-		echo esgmapfile $n [FAIL] $m | sendmail ames4@llnl.gov
+		echo esgmapfile $n [FAIL] $m $i
+		echo esgmapfile $n [FAIL] $m $i | sendmail ames4@llnl.gov
 		exit 1
 	fi
 
+	i=$(( $i + 1 ))
 done
 	       
