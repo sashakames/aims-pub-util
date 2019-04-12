b9df540c6086626921bf52747fcb2af70c53fb9c
Sasha Ames <ames4@aims3.llnl.gov>
2019-04-12 15:44:41 -0700

    add filename var

--- dset-status.sh
+++ dset-status.sh
@@ -1,8 +1,8 @@
 thedate=`date +%y%m%d`
-
+fn=/p/user_pub/publish-queue/dset-status/dset-count.$thedate.txt
 
 todo=`ls /p/user_pub/CMIP6-maps-todo/ | wc -l`
 donee=`ls /p/user_pub/CMIP6-maps-done/ | wc -l`
-echo $i $todo $donee >> dset-count.$thedate.txt
+echo $i $todo $donee >> $fn
 
 
