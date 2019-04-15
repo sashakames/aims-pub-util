60021b8ce1858252cd508a9697424caef94ad256
Sasha Ames <ames4@llnl.gov>
2019-04-15 11:56:47 -0700

    add sendmail on failure

--- mapfilegen-cmip6.sh
+++ mapfilegen-cmip6.sh
@@ -10,27 +10,27 @@ inidir=/export/ames4/pub/ini
 i=1
 
 for n in `cat $input_dir/$m` 
-
+do
     stop=`cat /tmp/map_status`
     
     if [ $stop == "true" ] ; then
     	echo Received Stop Notification, exiting before $m $i
+    fi
 
-	do 
-	echo RUN $n
-	if [ ! -d $n ] ; then 
-		echo missing perms or mount [FAIL] $m $i 
-		echo missing perms or mount [FAIL] $m $i | sendmail ames4@llnl.gov
-		exit 1
-	fi
-	esgmapfile -i $inidir --project cmip6 --outdir $outdir --max-processes 8 $n 
-
-	if [ $? -ne 0 ]; then
-		echo esgmapfile $n [FAIL] $m $i
-		echo esgmapfile $n [FAIL] $m $i | sendmail ames4@llnl.gov
-		exit 1
-	fi
-
-	i=$(( $i + 1 ))
+    echo RUN $n
+    if [ ! -d $n ] ; then 
+	echo missing perms or mount [FAIL] $m $i 
+	echo missing perms or mount [FAIL] $m $i | sendmail ames4@llnl.gov
+	exit 1
+    fi
+    esgmapfile -i $inidir --project cmip6 --outdir $outdir --max-processes 8 $n 
+    
+    if [ $? -ne 0 ]; then
+	echo esgmapfile $n [FAIL] $m $i
+	echo esgmapfile $n [FAIL] $m $i | sendmail ames4@llnl.gov
+	exit 1
+    fi
+    
+    i=$(( $i + 1 ))
 done
 	       
