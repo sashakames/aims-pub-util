5e52af2aa710d0556f4c73c0c3db90c3174118ff
Sasha Ames <ames4@llnl.gov>
2020-11-02 15:14:53 -0800

    correct location

--- renew-cert.sh
+++ renew-cert.sh
@@ -1,1 +1,4 @@
-echo 'Pass4esg*' | myproxy-logon -S -s esgf-node.llnl.gov -l sashakames -o ~/.globus/certificate-file -t 72
+cert=/export/ames4/.globus/certificate-file
+echo 'Pass4esg*' | myproxy-logon -S -s esgf-node.llnl.gov -l sashakames -o $cert -t 72
+chmod 64 $cert
+
