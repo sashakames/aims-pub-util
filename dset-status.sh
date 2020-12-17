038ad83c181b311ef901fe6bcf09cf6639738ee8
Sasha Ames <ames4@llnl.gov>
2020-12-17 11:37:07 -0800

    add list size count

--- dset-status.sh
+++ dset-status.sh
@@ -6,8 +6,17 @@ thetime=`date +%H%M`
 todo=`ls /p/user_pub/publish-queue/CMIP6-maps-todo/ | wc -l`
 donee=`ls /p/user_pub/publish-queue/CMIP6-maps-done/ | wc -l`
 err=`ls /p/user_pub/publish-queue/CMIP6-maps-err/ | wc -l`
-err2=`ls /p/user_pub/publish-queue/CMIP6-maps-err2/ | wc -l`
 
-echo $thetime $todo $donee $err $err2 >> $fn
+res=`ls /p/user_pub/publish-queue/CMIP6-list-todo | wc -l`
+
+if [ $res -gt 0 ] ; then
+
+    todo=`wc -l /p/user_pub/publish-queue/CMIP6-list-todo/* | tail -n1 | awk '{print $1}'`
+else
+
+    todo=0
+
+fi
+echo $thetime $todo $donee $err $todo >> $fn
 
 
